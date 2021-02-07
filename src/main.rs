9bb69137c6b3f82ce86b9dab0a82d9c3b23266e1
toidiu <apoorv@toidiu.com>
2021-02-07 01:59:28 -0800

    challenge 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 #![allow(unused)]
-use bytes::{Bytes, BytesMut};
+use bytes::{Buf, BufMut, Bytes, BytesMut};
 
 fn main() {}
 
 fn hex_to_bytes(hex: Bytes) -> Bytes {
     let bytes = hex::decode(hex).unwrap();
     Bytes::from(bytes)
+}
+
+fn bytes_to_hex(b: Bytes) -> Bytes {
+    Bytes::from(hex::encode(b))
 }
 
 fn bytes_to_base64(b: Bytes) -> String {
@@ -16,14 +20,12 @@ fn hex_to_base64(hex: Bytes) -> String {
     bytes_to_base64(hex_to_bytes(hex))
 }
 
-fn xor(a: Bytes, b: Bytes, out: &mut [u8]) {
+fn xor_bytes(a: Bytes, b: Bytes) -> Bytes {
+    let mut out = BytesMut::with_capacity(a.len());
     for (i, item) in a.iter().enumerate() {
-        // println!("====== {:?}", item);
-        // println!("====== {:?}", b[i]);
-        let uu: u8 = item ^ b[i];
-        // println!("----- {}", uu);
-        out[i] = item ^ b[i];
+        out.put_u8(item ^ b[i]);
     }
+    out.freeze()
 }
 
 #[cfg(test)]
@@ -32,33 +34,56 @@ mod test {
 
     #[test]
     fn xor_test() {
-        //     let x = Bytes::from("1c0111001f010100061a024b53535009181c");
-        //     let y = Bytes::from("686974207468652062756c6c277320657965");
-        //     let mut out = [0u8; 36];
-        //     println!("====== {:?}", x.as_ref());
+        let x_hex = Bytes::from("1c0111001f010100061a024b53535009181c");
+        let y_hex = Bytes::from("686974207468652062756c6c277320657965");
 
-        //     xor(x, y, &mut out);
-        //     let s = str::from_utf8(&out).unwrap();
-        //     assert_eq!(s, "746865206b696420646f6e277420706c6179");
+        let x = xor_bytes(hex_to_bytes(x_hex), hex_to_bytes(y_hex));
+        let s = bytes_to_hex(x);
+        assert_eq!(s, "746865206b696420646f6e277420706c6179");
+    }
+
+    #[test]
+    fn bytes_to_hex_test() {
+        let mut b = Bytes::from("0");
+        let mut x = bytes_to_hex(b);
+        assert_eq!(x, "30");
+
+        b = Bytes::from(":");
+        x = bytes_to_hex(b);
+        assert_eq!(x, "3a");
+
+        b = Bytes::from("the kid don't play");
+        x = bytes_to_hex(b);
+        assert_eq!(x, "746865206b696420646f6e277420706c6179");
     }
 
     #[test]
     fn hex_to_bytes_test() {
+        // decimal value
         let mut x = Bytes::from("00");
         let mut b = hex_to_bytes(x);
         assert_eq!(b.as_ref(), [0]);
-
-        x = Bytes::from("01");
-        b = hex_to_bytes(x);
-        assert_eq!(b.as_ref(), [1]);
 
         x = Bytes::from("ff");
         b = hex_to_bytes(x);
         assert_eq!(b.as_ref(), [255]);
 
+        // char value
+        x = Bytes::from("30");
+        b = hex_to_bytes(x);
+        assert_eq!(b, "0");
+
         x = Bytes::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
         b = hex_to_bytes(x);
         assert_eq!(b, "I'm killing your brain like a poisonous mushroom");
+    }
+
+    #[test]
+    fn hex_to_bytes_and_back() {
+        let b1 = Bytes::from("the kid don't play");
+        let x = bytes_to_hex(b1.clone());
+        let b2 = hex_to_bytes(x);
+        assert_eq!(b1, b2);
     }
 
     #[test]
